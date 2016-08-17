f0ab9f4933dcb32de377c7a0498b195852c4ef53
Joshua Porter <live.create.enjoy@gmail.com>
2016-08-17 17:48:16 +0000

    function-practice-1

---
+++ week2/function-practice-1/main.js
@@ -0,0 +1,52 @@
+// triple 5
+var triplefive
+for (triplefive = 0; triplefive <3; triplefive++){
+    console.log("Five!")
+}
+
+// lastLetter
+lastLetter("string")
+function lastLetter(string){
+    console.log(string.substring(string.length-1, string.length)) 
+}
+
+// square
+
+function square(num){
+    return num * num
+}
+
+// negate
+
+function negate(neg){
+    return neg * -1
+}
+
+// toArray
+function toArray(num1,num2,num3){
+    return [num1, num2, num3]
+}
+
+// startsWithA
+function startsWithA(){
+    
+}
+// excite
+function excite(words){
+    return words + "!!!"
+}
+
+// sun
+function sun(){
+    
+}
+
+// tiny
+function tiny(){
+    
+}
+
+// getSeconds
+function getSeconds(){
+    
+}
