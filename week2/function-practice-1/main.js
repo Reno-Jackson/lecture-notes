cf1499a639b9deb14acb5ef5cefba612d1595110
Joshua Porter <live.create.enjoy@gmail.com>
2016-08-18 18:42:47 +0000

    function-practice-1

--- week2/function-practice-1/main.js
+++ week2/function-practice-1/main.js
@@ -1,52 +1,70 @@
 // triple 5
 var triplefive
-for (triplefive = 0; triplefive <3; triplefive++){
+for (triplefive = 0; triplefive < 3; triplefive++) {
     console.log("Five!")
 }
 
 // lastLetter
 lastLetter("string")
-function lastLetter(string){
-    console.log(string.substring(string.length-1, string.length)) 
+
+function lastLetter(string) {
+    console.log(string.substring(string.length - 1, string.length))
 }
 
 // square
 
-function square(num){
+function square(num) {
     return num * num
 }
 
 // negate
 
-function negate(neg){
+function negate(neg) {
     return neg * -1
 }
 
 // toArray
-function toArray(num1,num2,num3){
+function toArray(num1, num2, num3) {
     return [num1, num2, num3]
 }
 
 // startsWithA
-function startsWithA(){
-    
+function startsWithA(word) {
+    if (word.substring(0, 1) == "a" || word.substring(0, 1) == "A") {
+        console.log("true");
+    }
+    else {
+        console.log("Use word with an A or a");
+    }
 }
 // excite
-function excite(words){
+function excite(words) {
     return words + "!!!"
 }
 
 // sun
-function sun(){
-    
+function sun(word) {
+    for (i = 0; i < word.length; i++) {
+        if (word.substring(i, i + 3) == "sun") {
+            console.log("True");
+            return ("True");
+        }
+    }
+    console.log("Type word with sun in it");
 }
-
 // tiny
-function tiny(){
-    
+function tiny(number) {
+    if (number > 0 && number < 1){
+    console.log("True")
+    return true
+    }
+    else {
+        return false
+    }
 }
 
 // getSeconds
-function getSeconds(){
-    
+function getSeconds(time) {
+    var str=time.split(":")
+    console.log ((str[0]*60)+(str[1]*1))
 }
