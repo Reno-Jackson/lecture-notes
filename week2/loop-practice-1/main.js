45cd5591e54b8a61a32a3c2f197c47384d92a262
Joshua Porter <live.create.enjoy@gmail.com>
2016-08-19 17:11:48 +0000

    loop-practice-1

---
+++ week2/loop-practice-1/main.js
@@ -0,0 +1,45 @@
+// For Loop Practice 1
+
+// Question 1.) 0 to 500 incrementing by the hundreds
+
+for( var i = 0; i <=500; i=i+100){
+    console.log(i);
+}
+
+// Question 2.) 1 to 64 doubling
+
+for (var i=1; i <=64; i=i*2){
+    console.log(i);
+}
+
+// Question 3.) 1 to 3 each number loops 3 times
+
+for (var i=1; i <=3; i++){
+    console.log(i);
+    console.log(i);
+    console.log(i);
+}
+
+// Question 4.) 0 to 10 incrementing by 2
+
+for(var i=0; i <=10; i=i+2){
+    console.log(i);
+}
+
+// Questions 5.) 3 to 15 incrementing by 3
+for(var i=3; i <=15; i=i+3){
+    console.log(i);
+}
+
+// Question 6.) 9 to 0 deccrementing by 1
+for( var i=9; i>=0; i--){
+    console.log(i);
+}
+
+// Question 7.) 1 to 3, incrementing by 1 looped 3 times
+
+for (var i=1; i <=3; i++){
+    for (var i1=1; i1 <=3; i1++){
+    console.log(i1);
+    }
+}
