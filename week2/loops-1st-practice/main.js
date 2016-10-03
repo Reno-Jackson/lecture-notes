094ae24f863b0bc8f4d5352205b3c9fde57d9e91
Reno-Jackson <live.create.enjoy@gmail.com>
2016-10-03 08:58:58 -0600

    Loop Practice Part Duex

---
+++ week2/loops-1st-practice/main.js
@@ -0,0 +1,41 @@
+// Loops Practice 1
+
+// Problem 1 Loop 0 to 500 incrementing by 100
+
+for(var i=0; i <=500; i+=100){
+    console.log(i);
+}
+
+// Problem 2 Loop from 1 to 64 doubling each times
+
+for(var i=1; i <= 64; i=i*2){
+    console.log(i);
+}
+
+//Problem 3 Make a loop that starts at 1, ends @ 3 and prints 1, 2, 3, 3 times
+for (var q=1; q<=3; q++){
+    for(var i=1; i <=3; i++){
+        console.log(q);
+    }
+}
+// Problem 4 Make a loop that starts @ 0, ends @ 10, increase by 2
+for(var i=0; i <=10; i=i+2){
+    console.log(i);
+}
+
+// Problem 5 Loop from 1 to 15 adding 3 each time
+for(var i=3; i <=15; i=i+3){
+    console.log(i);
+}
+
+// Problem 6 Loop from 9 to 0
+ for(var i =9; i >= 0; i--){
+     console.log(i);
+}
+
+// Problem 7 Loop 0-3 3 times, print out 0,1,2,3 3 times to console
+for (var q=1; q<=3; q++){
+    for(var i=1; i <=3; i++){
+        console.log(i);
+    }
+}
