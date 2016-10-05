6dc83393682687b6ce4039b1d904748faf841b4a
Reno-Jackson <live.create.enjoy@gmail.com>
2016-10-05 16:43:33 -0600

    Loop Practice Part II: The Loopening

---
+++ week3/loop-practice-2/loopsII.js
@@ -0,0 +1,29 @@
+//////////////// Loop Practice Part 2////////
+
+//var animals = ['rat', 'cat', 'butterfly', 'marmot', 'ocelot'];
+// for(var i=0; i < animals.length; i++) {
+//  console.log(animals[i]);
+// }
+
+
+// rat cat butterfly marmot
+var animals = ['rat', 'cat', 'butterfly', 'marmot'];
+for (var i = 0; i < animals.length; i++) {
+    console.log(animals[i]);
+}
+// rat butterfly ocelot
+var animals = ['rat', 'cat', 'ocelot'];
+for (var i = 0; i < animals.length; i++) {
+    console.log(animals[i]);
+}
+// ocelot marmot butterfly cat rat
+var animals = ['ocelot', 'marmot', 'butterfly', 'cat', 'rat'];
+for (var i = 0; i < animals.length; i++) {
+    console.log(animals[i]);
+}
+
+// rat cat cat butterfly butterfly marmot marmot ocelot
+var animals = ['rat', 'cat', 'cat', 'butterfly', 'butterfly', 'marmot', 'marmot', 'ocelot'];
+for (var i = 0; i < animals.length; i++) {
+    console.log(animals[i]);
+}
